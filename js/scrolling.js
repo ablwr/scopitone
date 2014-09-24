70a1277d94ca21890792fff69eabc24947cfa178
ablwr <ashley.blewer@gmail.com>
2014-09-23 21:37:18 -0400

    remove bottom alert test

--- js/scrolling.js
+++ js/scrolling.js
@@ -84,6 +84,6 @@ function getDocHeight() {
 
 $(window).scroll(function() {
        if($(window).scrollTop() + $(window).height() == getDocHeight()) {
-           alert("bottom!");
+           scrollActivate;
        };
    });
